661c1e24615dbf22894efaf848d3aa2c82186f80
wt <1329785954@qq.com>
2018-04-15 21:39:10 +0800

    加入redis

--- manage.py
+++ manage.py
@@ -1,23 +1,29 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_redis import redis
+import redis
 
 
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.147.3:3306/ihome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
 
 @app.route('/')
 def hello_world():
+    redis_store.set('name', 'laowang')
     return 'Hello World!'
 
 
