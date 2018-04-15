5261198dcac7679ed9e5b763af2ddd17b388fc56
wt <1329785954@qq.com>
2018-04-15 18:54:28 +0800

    集成mysql数据库

--- flaskIHome.py
+++ flaskIHome.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.147.3:3306/ihome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
